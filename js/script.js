3914e7900c2a9fda4aef17570eab503e349f690e
Pavithran R <pavithranr.16cse@gmail.com>
2024-12-21 00:45:37 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  console.log("Document is ready!");
+
+  $(".view-products-btn").on("click", function () {
+    console.log("View Products button clicked!");
+    $(".dashboard-container").hide();
+    $(".product-card").show();
+    console.log("Product card should now be visible.");
+  });
+
+  $(".close-icon").on("click", function () {
+    console.log("Close icon clicked!");
+    $(".product-card").hide();
+    $(".dashboard-container").show();
+    console.log("Dashboard cards should now be visible.");
+  });
+});
