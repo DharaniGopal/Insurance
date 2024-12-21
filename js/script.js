339f1aac03edd57295958670a1354977b9fe7a05
dharanigopal <dharanigopal100897@gmail.com>
2024-12-21 16:47:41 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   console.log("Document is ready!");
 
+  // Hide the product card initially
+  $(".product-card").hide();
+
   $(".view-products-btn").on("click", function () {
     console.log("View Products button clicked!");
     $(".dashboard-container").hide();
