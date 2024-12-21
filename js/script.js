68547f0caa3139b5709cf326eb1c357286c01fc2
DharaniGopal <131185167+DharaniGopal@users.noreply.github.com>
2024-12-21 17:05:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   console.log("Document is ready!");
-
-  // Hide the product card initially
   $(".product-card").hide();
 
   $(".view-products-btn").on("click", function () {
     console.log("View Products button clicked!");
     $(".dashboard-container").hide();
     $(".product-card").show();
+    $(".image-quote-container ").css("bottom", "-340px");
     console.log("Product card should now be visible.");
   });
 
@@ -15,6 +14,7 @@ $(document).ready(function () {
     console.log("Close icon clicked!");
     $(".product-card").hide();
     $(".dashboard-container").show();
+    $(".image-quote-container ").css("bottom", "-200px");
     console.log("Dashboard cards should now be visible.");
   });
 });
